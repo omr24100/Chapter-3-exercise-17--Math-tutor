1b81e592eb75cd48d6afb84aa5d29718ec7f504a
Olivia Ruiz <omr24100@email.vccs.edu>
2025-02-11 13:09:14 -0500

    Update Chapter 3 exercise 17- Math tutor

--- Chapter 3 exercise 17- Math tutor.cpp
+++ Chapter 3 exercise 17- Math tutor.cpp
@@ -2,7 +2,7 @@
 //
 /*
 File Name: Chapter 3 exercise 17- Math tutor
-GitHub:
+GitHub:https://github.com/omr24100/Chapter-3-exercise-17--Math-tutor.git
 Programmer: Olivia Ruiz
 Date: 2/11/25
 Requirments: Write a program that can be used as a math tutor for a young student. The program should display two random numbers to be added, such as:
